e17f1a845b9c7af06bee25803f6c76018638a27f
xuwei <xuwei@thvr.com>
2023-11-20 20:03:38 +0800

    update setenv.cmd

--- SetEnv.cmd
+++ SetEnv.cmd
@@ -1,4 +1,7 @@
-@echo off
-setx -m PATH "%PATH%;C:\Program Files\CodeAndWeb\TexturePacker\;C:\Program Files\WinRAR"
-echo set all env done
+%1 mshta vbscript:createobject("shell.application").shellexecute("%~s0","::","","runas",1)(window.close)&exit
+cd /d %~dp0 
+
+setx /M PATH "%PATH%;C:\Program Files\CodeAndWeb\TexturePacker\bin;C:\Program Files\WinRAR"
+echo set all env done:
+echo %PATH%
 pause
